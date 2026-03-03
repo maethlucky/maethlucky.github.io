c8e969df8296962b60b9055aae76a54594816cca
maethlucky <123992696+maethlucky@users.noreply.github.com>
2026-03-02 22:29:50 -0800

    Updated home page with other projects, images, and an about me section

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Global variables
-let mouseCircle = document.querySelector("#mouseCircle");
+let darkMode = document.querySelector("#darkMode");
+let overlay = document.querySelector("#popupOverlay");
 
-// Event listeners
-document.addEventListener("mousemove", function(e) {
-    mouseCircle.style.left = e.clientX;
-    mouseCircle.style.top = e.clientY;
-})
+document.querySelector("#darkModeSwitch").addEventListener("click", toggleDarkMode);
+document.querySelector("#aboutBtn").addEventListener("click", showAboutWindow);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Escape") overlay.style.display = "none";
+});
+
+function showAboutWindow() {
+    console.log("Displaying pop-up");
+    overlay.style.display = "flex";
+}
+
+function toggleDarkMode() {
+    darkMode.style.backgroundColor = document.querySelector("#darkModeBox").checked ? "white" : "black";
+}
